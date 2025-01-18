9e4b6789435ed1a11e370bb0f3a513c5a11823ca
Usenko, Dmytro <du.usenko.du@gmail.com>
2025-01-18 15:19:35 +0200

    Removed Main.storyboard
    Removed Main.storyboard and ViewController

--- TransactionsTestTask/InitialDelegates/SceneDelegate.swift
+++ TransactionsTestTask/InitialDelegates/SceneDelegate.swift
@@ -10,15 +10,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    func scene(
-        _ scene: UIScene,
-        willConnectTo session: UISceneSession,
-        options connectionOptions: UIScene.ConnectionOptions
-    ) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions){
+        self.createWindow(with: scene)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -49,5 +44,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
+    }
+}
+
+extension SceneDelegate {
+    
+    private func createWindow(with scene: UIScene) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let mainViewController = UIViewController()
+        mainViewController.view.backgroundColor = .cyan
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        
+        window.rootViewController = navigationController
+        self.window = window
+        window.makeKeyAndVisible()
     }
 }
