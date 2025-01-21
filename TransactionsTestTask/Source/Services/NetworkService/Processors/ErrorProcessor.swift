ef1f2760525a77a2bb1a5d6866774035fcc7dea9
Usenko, Dmytro <du.usenko.du@gmail.com>
2025-01-21 14:47:50 +0200

    Moved NetworkErrors to separate file
    Moved NetworkErrors to separate file

--- TransactionsTestTask/Source/Services/NetworkService/Processors/ErrorProcessor.swift
+++ TransactionsTestTask/Source/Services/NetworkService/Processors/ErrorProcessor.swift
@@ -7,42 +7,6 @@
 
 import Foundation
 
-enum NetworkErrors: LocalizedError {
-    
-    case invalidURL
-    case invalidData
-    case requestFailed(statusCode: Int)
-    case serverError(statusCode: Int)
-    case jsonParsingFailure
-    case responseUnsuccessful
-    case notConnectedToInternet
-    case timeOut
-    case unexpected(error: String? = "")
-    
-    var errorDescription: String {
-        switch self {
-        case .invalidURL:
-            return LocalizationConstants.NetworkErrors.invalidURL
-        case .invalidData:
-            return LocalizationConstants.NetworkErrors.invalidData
-        case .requestFailed(let code):
-            return LocalizationConstants.NetworkErrors.requestFailed + code.description
-        case .jsonParsingFailure:
-            return LocalizationConstants.NetworkErrors.jsonParsingFailure
-        case .responseUnsuccessful:
-            return LocalizationConstants.NetworkErrors.responseUnsuccessful
-        case .notConnectedToInternet:
-            return LocalizationConstants.NetworkErrors.notConnectedToInternet
-        case .timeOut:
-            return LocalizationConstants.NetworkErrors.responseUnsuccessful
-        case .unexpected(let error):
-            return LocalizationConstants.NetworkErrors.unexpectedError + (error ?? "")
-        case .serverError(let code):
-            return LocalizationConstants.NetworkErrors.requestFailed + code.description
-        }
-    }
-}
-
 // MARK: - Error Processor Protocol
 
 protocol ErrorProcessor {
