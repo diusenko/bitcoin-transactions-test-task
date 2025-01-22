17bd39b4759c281d3aa3d0124ee3d39b0251ca74
Usenko, Dmytro <du.usenko.du@gmail.com>
2025-01-22 22:17:10 +0200

    Changed type for balance from int to float

--- TransactionsTestTask/Source/Entities/PresentationModels/BalancePresentationModel.swift
+++ TransactionsTestTask/Source/Entities/PresentationModels/BalancePresentationModel.swift
@@ -1,12 +1,10 @@
 //
-//  AccountBalanceEntity.swift
+//  BalancePresentationModel.swift
 //  TransactionsTestTask
 //
 //  Created by Dmytro Usenko on 22.01.2025.
 //
 
-import Foundation
-
-struct AccountBalance: Decodable {
+struct BalancePresentationModel {
     let balance: Float
 }
